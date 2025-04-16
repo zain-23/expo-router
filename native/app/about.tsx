6b23d178e6b20d78317e858e045c178fd0c13f82
zain-23 <zaingit780@gmail.com>
2025-04-16 12:39:28 +0500

    Add About and Profile pages with basic layout and navigation links

---
+++ native/app/about.tsx
@@ -0,0 +1,18 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+
+export default function About() {
+  return (
+    <View style={styles.container}>
+      <Text>About Page</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+});
--- native/app/index.tsx
+++ native/app/index.tsx
@@ -1,15 +1,19 @@
-import { Text, View } from "react-native";
+import { Link } from 'expo-router';
+import { Text, View } from 'react-native';
 
-export default function Index() {
+export default function Home() {
   return (
     <View
       style={{
         flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
+        justifyContent: 'center',
+        alignItems: 'center',
+        gap: 10
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text>Home Page</Text>
+      <Link href='/about'>About Page</Link>
+      <Link href='/profile/index'>Profile Page</Link>
     </View>
   );
 }
