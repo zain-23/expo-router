6e2f6eefbc5e16dcbff25d0c44cba9d13ddd448f
zain-23 <zaingit780@gmail.com>
2025-04-21 16:56:05 +0500

    Remove unused route link from Home component and add login and signup pages; learn route group

--- native/app/index.tsx
+++ native/app/index.tsx
@@ -15,7 +15,6 @@ export default function Home() {
       <Link href='/about'>About Page</Link>
       <Link href='/profile'>Profile Page</Link>
       <Link href='/products'>Products Page</Link>
-      <Link href='/router'>Route Not Exist</Link>
     </View>
   );
 }
