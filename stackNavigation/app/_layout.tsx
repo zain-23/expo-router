5c9b7415668fca63f616ebafbcaba018ad9467eb
zain-23 <zaingit780@gmail.com>
2025-04-22 12:58:45 +0500

    Enhance RootLayout with header menu button and add About screen

--- stackNavigation/app/_layout.tsx
+++ stackNavigation/app/_layout.tsx
@@ -1,5 +1,40 @@
-import { Stack } from "expo-router";
+import { Stack } from 'expo-router';
+import { Pressable, Text } from 'react-native';
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <Stack>
+      <Stack.Screen
+        name='index'
+        options={{
+          title: 'Home',
+          headerStyle: {
+            backgroundColor: '#6a51ae'
+          },
+          headerTintColor: '#fff',
+          headerTitleStyle: {
+            fontWeight: 'bold'
+          },
+          headerRight: () => (
+            <Pressable onPress={() => alert('Menu button click')}>
+              <Text
+                style={{
+                  color: '#fff',
+                  fontSize: 16
+                }}
+              >
+                Menu
+              </Text>
+            </Pressable>
+          )
+        }}
+      />
+      <Stack.Screen
+        name='about'
+        options={{
+          title: 'About'
+        }}
+      />
+    </Stack>
+  );
 }
---
+++ stackNavigation/app/about.tsx
@@ -0,0 +1,18 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+
+const About = () => {
+  return (
+    <View>
+      <Text
+        style={{
+          fontSize: 30
+        }}
+      >
+        About
+      </Text>
+    </View>
+  );
+};
+
+export default About;
